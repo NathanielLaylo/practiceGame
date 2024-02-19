c71bd60c75e22989cadbf48081305bc1067a870e
NathanielLaylo <laylonathan1@yahoo.com>
2024-02-19 19:13:46 +0000

    .

--- practiceGame/src/com/practiceGame/Main.java
+++ practiceGame/src/com/practiceGame/Main.java
@@ -8,14 +8,21 @@ public class Main {
     public static void main(String[] args) {
         Character matiyo = new Character();
         Monster goblin = new Monster();
+<<<<<<< Updated upstream
         matiyo.name = "wow";
         //matiyo.fightMonster(goblin);
         System.out.println(matiyo.name);
+=======
+        goblin.monsterName = "Goblin";
+        character.playerName = "Matiyo";
+        System.out.println(character.playerName);
+>>>>>>> Stashed changes
         boolean playingStatus = true;
         Scanner scanner = new Scanner(System.in);
 
         while(playingStatus){
-            System.out.println("Type \"fight\" to battle monsters! ");
+            System.out.println("Type \"fight\" to battle monsters!\n " +
+                    "    \"quit\" to exit the game!");
             String userInput = scanner.nextLine().toLowerCase();
             
 
