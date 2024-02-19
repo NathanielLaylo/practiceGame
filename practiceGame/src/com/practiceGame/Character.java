0625f8ee479b19a528d1ef5c1f380f56c7499b4e
NathanielLaylo <laylonathan1@yahoo.com>
2024-02-19 19:48:49 +0000

    character methods
    added character methods
    -for earning exp
    -earning coins
    -adding stats
    -leveling up

--- practiceGame/src/com/practiceGame/Character.java
+++ practiceGame/src/com/practiceGame/Character.java
@@ -1,6 +1,7 @@
 package com.practiceGame;
 import com.practiceGame.Monster;
 class Character {
+    int characterLevel = 1;
     final int MAX_HEALTH  = 100;
     int currentHealth = MAX_HEALTH;
     final int MAX_EXP = 100;
@@ -8,16 +9,49 @@ class Character {
     int attackPower = 1;
     int defencePower = 1;
     int goldCoins = 1;
+    int statPoints = 0;
     String playerName = "";
 
+    //earn exp by defeating monster
+    public void earnExp(Monster monster){
+        currentExp = currentExp + monster.rewardExp;
+        System.out.println("Your current exp: " + currentExp + "/" + MAX_EXP);
+        if(currentExp >= MAX_EXP){
+            characterLevel++;
+            statPoints = statPoints + 2;
+            System.out.println("You leveled UP!!");
+        }
+    }
+
+    //earn coins by defeating monster
+    public  void earnCoins(Monster monster){
+        goldCoins = goldCoins + monster.rewardCoins;
+        System.out.println("Your current gold: " + goldCoins);
+    }
+
+    //increase stats using stat points
+    public void increaseAttack(){
+        if (statPoints > 0){
+            attackPower++;
+            System.out.println("Current attack power: " + attackPower);
+        }
+    }
+    public void increaseDefence(){
+        if (statPoints > 0){
+            defencePower++;
+            System.out.println("Current defence power: " + defencePower);
+        }
+    }
+
     public void fightMonster(Monster monster){
+        int monsterHealth = monster.health;
         System.out.println(playerName + " VS " + monster.monsterName);
-        while (currentHealth > 0 && monster.currentHealth > 0){
-            monster.currentHealth = monster.currentHealth - (attackPower - monster.defencePower);
+        while (currentHealth > 0 && monsterHealth > 0){
+            monsterHealth = monsterHealth - (attackPower - monster.defencePower);
             currentHealth = currentHealth - (monster.attackPower- defencePower);
-            if (monster.currentHealth <= 0){
-                currentExp = currentExp + monster.rewardExp;
-                goldCoins = goldCoins + monster.rewardCoins;
+            if (monsterHealth <= 0){
+                earnExp(monster);
+                earnCoins(monster);
                 System.out.println("Monster Dead!\nYou earned " + monster.rewardExp + " Exp " +
                         "and " + monster.rewardCoins + " Coin(s)!");
             } else if (currentHealth <= 0){
--- practiceGame/src/com/practiceGame/Monster.java
+++ practiceGame/src/com/practiceGame/Monster.java
@@ -1,8 +1,8 @@
 package com.practiceGame;
 
 public class Monster {
-    int currentHealth = 100;
-    int rewardExp = 5;
+    int health = 100;
+    int rewardExp = 10;
     int attackPower = 1;
     int defencePower = 0;
     int rewardCoins = 1;
